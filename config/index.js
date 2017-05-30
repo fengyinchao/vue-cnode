9a088590ff3c0b7c8614ed0a3d85eceed54829d5
fengyinchao <fengyinchao.hust@gmail.com>
2017-05-30 09:23:10 +0800

    modify name

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath:  '/v-cnode/dist',
+    assetsPublicPath:  '/vue-cnode/dist',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
